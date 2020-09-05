bb254f388fff163b8c780759e0d85b03d05a07ab
anil <anil@ncodeit.com>
2020-09-05 03:12:22 -0400

    script to push code to github using username added

---
+++ git-push-with-username.sh
@@ -0,0 +1,7 @@
+git config user.name "anil"
+git config user.email "anil@ncodeit.com"
+#git remote add origin git@github.com:ncodeitgithub2/everyday-scripts.git
+git config credential.helper store
+git pull https://github.com/ncodeitgithub2/everyday-scripts.git
+git status
+git push https://github.com/ncodeitgithub2/everyday-scripts.git
